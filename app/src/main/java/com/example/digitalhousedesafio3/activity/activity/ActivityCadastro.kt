b874c9ed52d23e6e148c0155f4b319860300cb50
NayaraGarcia <70548587+NayaraGarcia@users.noreply.github.com>
2020-12-02 22:37:57 -0300

    Layout Cadastro e Class Cadastro

--- app/src/main/java/com/example/digitalhousedesafio3/activity/activity/ActivityCadastro.kt
+++ app/src/main/java/com/example/digitalhousedesafio3/activity/activity/ActivityCadastro.kt
@@ -1,4 +1,23 @@
 package com.example.digitalhousedesafio3.activity.activity
 
-class ActivityCadastro {
-}
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.digitalhousedesafio3.R
+import kotlinx.android.synthetic.main.activity_cadastro.*
+
+class ActivityCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+
+        inputPassword.transformationMethod =
+            AsteriskPassword()
+
+        toolbar.setNavigationOnClickListener {
+            this.onBackPressed()
+        }
+
+
+        }
+    }
