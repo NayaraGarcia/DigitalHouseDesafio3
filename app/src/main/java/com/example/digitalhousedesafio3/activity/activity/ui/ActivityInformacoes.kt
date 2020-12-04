f78b3bab97a37c3e56e9d28263825e902089ac20
NayaraGarcia <70548587+NayaraGarcia@users.noreply.github.com>
2020-12-04 14:35:51 -0300

    Activity Informaçõe sobre herói

---
+++ app/src/main/java/com/example/digitalhousedesafio3/activity/activity/ui/ActivityInformacoes.kt
@@ -0,0 +1,12 @@
+package com.example.digitalhousedesafio3.activity.activity.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.digitalhousedesafio3.R
+
+class ActivityInformacoes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_informacoes)
+
+    }}
